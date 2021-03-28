cec61fc2005daa803350b8b1dcf1bd975b114a93
Sergio <S.suarerez@gmail.com>
2021-03-27 18:17:07 -0600

    add perceptron

--- src/actividad_2/main.py
+++ src/actividad_2/main.py
@@ -38,27 +38,46 @@ def draw_2d(model):
         '--k')
     plt.show()
 
-X = np.array([[0, 0, 1, 1],
-              [0, 1, 0, 1]])
+elem = 100
 
-Y = np.array([0, 1, 1, 0])
+X = np.array(([np.random.uniform(1.40, 2.1) for i in range(elem)],
+              [np.random.randint(35, 120) for i in range(elem)]))
+
+imc = np.array([X[1, i] / X[0, i]**2 for i in range(elem)])
+
+temp = []
+for i in range(elem):
+    if imc[i] > 25:
+        temp.append(1)
+    else:
+        temp.append(0)
+
+Y = np.array(temp)
+
+altura_maxima = X[0].min()
+altura_minima = X[0].max()
+masa_maxima = X[1].min()
+masa_minima = X[1].max()
+
+X = np.array(([((X[0, i] - altura_minima) / (altura_maxima - altura_minima)) for i in range(elem)],
+            [((X[1, i] - masa_minima) / (masa_maxima - masa_minima)) for i in range(elem)]))
 
 neuron = Perceptron(n_input=2, learning_rate=0.1)
 
 neuron.train(X, Y)
 
 _, p = X.shape
-
 for i in range(p):
     if Y[i] == 0:
         plt.plot(X[0, i], X[1, i], 'or')
     else:
         plt.plot(X[0, i], X[1, i], 'ob')
+    
 
-plt.title("Perceptrón XOR")
+plt.title("Perceptrón IMC")
 plt.grid('on')
-plt.xlim([-2, 2])
-plt.ylim([-2, 2])
+plt.xlim([0, 1])
+plt.ylim([0, 1])
 plt.xlabel(r'$x_1$')
 plt.xlabel(r'$x_2$')
 
